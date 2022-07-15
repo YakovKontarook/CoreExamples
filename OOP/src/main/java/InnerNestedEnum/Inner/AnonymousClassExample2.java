a259e3afa4ce7f4e7895209e9b91c10169d9cca5
Yakov Kontarook <88117408+Woodcreek@users.noreply.github.com>
2022-07-15 13:11:51 +0300

    Java Core Examples

---
+++ OOP/src/main/java/InnerNestedEnum/Inner/AnonymousClassExample2.java
@@ -0,0 +1,22 @@
+package InnerNestedEnum.Inner;
+
+public class AnonymousClassExample2 {
+    public static void main(String[] args) {
+        AbstractClass abstractClass = new AbstractClass(10) {
+            @Override
+            public int getValue() {
+                return super.getValue() + 1;
+            }
+        };
+    }
+}
+
+abstract class AbstractClass {
+    private final int value;
+    protected AbstractClass(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
